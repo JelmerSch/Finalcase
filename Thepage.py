f82b1b251b6dd842fe0db4afb7f77719dc4df145
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-14 16:51:53 +0200

    Update Thepage.py

--- Thepage.py
+++ Thepage.py
@@ -1,1 +1,35 @@
+#### opstarten
 
+
+#### Imports
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#### cache
+
+
+#### session status
+
+
+
+#### hervormen data
+
+
+#### Begin TAB
+Tab_1, Tab_2, Tab_3 = st.tabs(["Hoofdpagina", "Analyse", "Resultaat"])
+
+#### TAB 1 hoofdpagina + intro
+with Tab_1:
+  st.write("start")
+
+#### TAB 2 diepere analyse
+with Tab_2:
+  st.write("analyse")
+
+#### TAB 3 resultaten en conclusie
+with Tab_3:
+  st.write("resultaten")
+
+  
+#### einde script
