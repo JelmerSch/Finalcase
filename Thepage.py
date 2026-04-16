f63da66d8e7921dee60fa1a93f0fbfedfabac7cb
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-16 17:06:05 +0200

    Update Thepage.py

--- Thepage.py
+++ Thepage.py
@@ -26,7 +26,7 @@ def load_Disasters(disasters_data):
 
 #### hervormen data
 Fao_productie = load_fao_productie("fao_data_production_indices_data.csv.zip")
-Fao_crops = load_fao_crops("fao_data_crops_data")
+Fao_crops = load_fao_crops("fao_data_crops_data.csv.zip")
 rampen = load_Disasters("1900_2021_DISASTERS.csv.zip")
 
 #### pagina indeling
