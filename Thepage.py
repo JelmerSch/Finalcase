455b9704e8cb81f611c6f3065d8c68def7f606b9
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-16 16:05:34 +0200

    testen

--- Thepage.py
+++ Thepage.py
@@ -4,11 +4,21 @@ import pandas as pd
 import numpy as np
 
 
-
-Fao_crops_data = pd.read_csv("fao_data_crops_data.csv.zip", compression='zip')
-
 #### cache
+@st.cache_data(show_spinner="Fao productie laden")
+def load_fao_productie(fao_data_production_indices_data):
+  Fao_productie = pd.read_csv("fao_data_production_indices_data.csv.zip", compression='zip')
+  return Fao_productie
 
+@st.cache_data(show_spinner="Fao crops laden")
+def load_fao_crops(fao_data_crops_data):
+  Fao_crops = pd.read_csv("fao_data_crops_data.csv.zip", compression='zip')
+  return Fao_crops
+
+@st.cache_data(show_spinner="Rampen laden")
+def load_Disasters(disasters_data):
+  Rampen = pd.read_csv("1900_2021_DISASTERS.csv.zip", compression='zip')
+  return Rampen
 
 #### session status
 
@@ -30,7 +40,7 @@ with Tab_1:
 #### TAB 2 diepere analyse
 with Tab_2:
   st.write("analyse")
-  st.dataframe(Fao_crops_data.head(100))
+  st.dataframe(Fao_crops.head(100))
   
 #### TAB 3 resultaten en conclusie
 with Tab_3:
