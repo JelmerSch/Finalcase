4873235e46b07d263a3f284045bffdcefd379285
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-16 17:15:13 +0200

    klok

--- Thepage.py
+++ Thepage.py
@@ -26,8 +26,8 @@ def load_Disasters(disasters_data):
 
 #### hervormen data
 Fao_productie = load_fao_productie("fao_data_production_indices_data.csv.zip")
-Fao_crops = load_fao_crops("fao_data_crops_data")
-rampen = load_Disasters("1900_2021_DISASTERS.csv.zip")
+Fao_crops = load_fao_crops("fao_data_crops_data.csv.zip")
+rampen = load_Disasters("1900_2021_DISASTERS.xlsx - emdat data.csv.zip")
 
 #### pagina indeling
 st.set_page_config(layout="centered")
@@ -37,20 +37,23 @@ Tab_1, Tab_2, Tab_3 = st.tabs(["Hoofdpagina", "Analyse", "Resultaat"])
 
 #### TAB 1 hoofdpagina + intro
 with Tab_1:
-  st.write("start")
+  st.write("Start")
 
 #### TAB 2 diepere analyse
 with Tab_2:
-  st.write("analyse")
+  st.write("Analyse")
+  st.write("Productie")
   st.dataframe(Fao_productie.head(500))
-  st.divider
+  st.divider()
+  st.write("Crops")
   st.dataframe(Fao_crops.head(500))
-  st.divider
+  st.divider()
+  st.write("Rampen")
   st.dataframe(rampen.head(500))
   
 #### TAB 3 resultaten en conclusie
 with Tab_3:
-  st.write("resultaten")
+  st.write("Resultaten")
 
   
 #### einde script
