5eaffdbc9a396532715cdc11d5d0d3ba8414c01f
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-17 16:25:22 +0200

    extra's

--- Thepage.py
+++ Thepage.py
@@ -3,29 +3,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
 #### pagina indeling
 st.set_page_config(layout="centered")
 
 #### cache
-@st.cache_data(show_spinner="Fao data laden")
-def load_FAO(FAOSTAT_data):
-  FAO_data = pd.read_csv(FAOSTAT_data)
-  return FAO_data
+@st.cache_data(show_spinner="Wereld Fao")
+def Wereld_FAO(FAO_wereld):
+  FAO_Wereld_data = pd.read_csv(FAO_wereld)
+  return FAO_Wereld_data
 
-@st.cache_data(show_spinner="Rampen laden")
-def load_Disasters(disasters_data):
-  Rampen = pd.read_csv(disasters_data, compression='zip')
-  return Rampen
-
-@st.cache_data(show_spinner="Pivotten Fao")
-def pivot_FAO(FAO_data):
-  FAO_pivot = FAO_data.pivot_table(
-    index=['Domain Code', 'Domain', 'Area Code (M49)', 'Area', 'Item Code (CPC)',
-           'Item', 'Year Code', 'Year', 'Flag', 'Flag Description'],
+@st.cache_data(show_spinner="Wereld Fao pivot")
+def Wereld_FAO_pivot(FAO_Wereld_data):
+  FAO_Wereld_data_pivot = FAO_Wereld_data.pivot_tabel(
+    index=['Area Code (M49)', 'Area', 'Item', 'Year', 'Flag', 'Flag Description'],
     columns='Element',
     values=['Unit', 'Value'],
-    aggfunctie='first',
+    aggfunc='first',
   ).reset_index()
 
   #aanpassen en toevoegen kolommen units en values pivot
@@ -34,23 +27,56 @@ def pivot_FAO(FAO_data):
     for col in FAO_pivot.columns]
 
   #nieuwe kolom voor totale yield
-  FAO_pivot['Yield Quantities'] = FAO_pivot['Value_Area harvested'] * FAO_pivot['Value_Yield']
+  FAO_pivot['Yield Quantities'] = (FAO_pivot['Value_Area harvested'] * FAO_pivot['Value_Yield']) / 1000
+  FAO_pivot['Unit_Yield Quantities'] = "t"
+  return FAO_Wereld_data_pivot
 
+@st.cache_data(show_spinner="Rampen laden")
+def load_Disasters(disasters_data):
+  Rampen = pd.read_csv(disasters_data, compression='zip')
+  return Rampen
+
+@st.cache_data(show_spinner="Fao data laden")
+def load_FAO(FAOSTAT_data):
+  FAO_data = pd.read_csv(FAOSTAT_data)
+  return FAO_data
+
+@st.cache_data(show_spinner="Pivotten Fao")
+def pivot_FAO(FAO_data):
+  FAO_pivot = FAO_data.pivot_table(
+    index=['Area Code (M49)', 'Area', 'Item', 'Year', 'Flag', 'Flag Description'],
+    columns='Element',
+    values=['Unit', 'Value'],
+    aggfunc='first',
+  ).reset_index()
+
+  #aanpassen en toevoegen kolommen units en values pivot
+  FAO_pivot.columns = [
+    '_'.join(col).strip('_') if col[1] else col[0]
+    for col in FAO_pivot.columns]
+
+  #nieuwe kolom voor totale yield
+  FAO_pivot['Yield Quantities'] = (FAO_pivot['Value_Area harvested'] * FAO_pivot['Value_Yield']) / 1000
+  FAO_pivot['Unit_Yield Quantities'] = "t"
   return FAO_pivot
 
 #### session status
-if "FAO_data" not in st.session_state:
-    st.session_state["FAO_data"] = load_FAO("FAOSTAT_data_en_4-17-2026.csv")
-
+if "FAO_Wereld_data" not in st.session_state:
+    st.session_state["FAO_Wereld_data"] = load_FAO("FAOSTAT_wereld_data_en_4-17-2026.csv")
+if "FAO_Wereld_data_pivot" not in st.session_state:
+    st.session_state["FAO_Wereld_data_pivot"] = pivot_FAO(st.session_state["FAO_Wereld_data"])
 if "Rampen" not in st.session_state:
     st.session_state["Rampen"] = load_Disasters("1900_2021_DISASTERS.xlsx - emdat data.csv.zip")
-
+if "FAO_data" not in st.session_state:
+    st.session_state["FAO_data"] = load_FAO("FAOSTAT_data_en_4-17-2026.csv")
 if "FAO_pivot" not in st.session_state:
     st.session_state["FAO_pivot"] = pivot_FAO(st.session_state["FAO_data"])
 
 #### Inladen data vanuit session state
 Fao_data = st.session_state["FAO_data"]
 Fao_pivot = st.session_state["FAO_pivot"]
+Fao_wereld_data = st.session_state["FAO_Wereld_data"]
+Fao_wereld_pivot = st.session_state["FAO_Wereld_data_pivot"]
 rampen = st.session_state["Rampen"]
 
 #### Begin TAB
@@ -65,9 +91,15 @@ with Tab_2:
   st.write("Analyse")
   st.write("Productie en yield")
   st.dataframe(Fao_data.head(1000))
-  st.divider
+  st.divider()
   st.write("Pivot van data")
   st.dataframe(Fao_pivot.head(500))
+  st.divider()
+  st.write("Productie en yield")
+  st.dataframe(Fao_wereld_data.head(1000))
+  st.divider()
+  st.write("Pivot van data")
+  st.dataframe(Fao_wereld_pivot.head(500))
   st.divider()
   st.write("Rampen")
   st.dataframe(rampen.head(1000))
@@ -77,4 +109,4 @@ with Tab_3:
   st.write("Resultaten")
 
   
-#### einde script
+#### Einde script
