ee830c7d00d3751a715d7640d555b31f08f0e1b9
JelmerSch <jelmerschaaphok@gmail.com>
2026-04-19 20:47:22 +0200

    mappen test4

--- Thepage.py
+++ Thepage.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
 import folium
-from folium.plugins import LayerControl
+from folium import LayerControl
 import branca.colormap as cm
 from streamlit_folium import st_folium
 
@@ -195,10 +195,10 @@ def build_folium_map(fao_pivot_clean, rampen_clean):
 
         productie_dict = dict(zip(df_g['Area'], df_g['Gem_Productie']))
 
-        def style_graan(feature, pd=productie_dict, cmap=colormap):
+        def style_graan(feature, pdict=productie_dict, cmap=colormap):
             naam = feature['properties'].get('name', '')
-            val = pd.get(naam, None)
-            if val is None or pd.isna(val):
+            val = pdict.get(naam, None)
+            if val is None or (isinstance(val, float) and val != val):
                 return {'fillColor': '#d3d3d3', 'color': '#555', 'weight': 0.5,
                         'fillOpacity': 0.4}
             return {'fillColor': cmap(val), 'color': '#555', 'weight': 0.5,
